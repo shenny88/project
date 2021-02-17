74ef0f014170c8b232897add8d1ae30c6c7b84ce
shennyrs <shenny88@gmail.com>
2021-02-17 18:55:37 +0530

    adding testing script

---
+++ testing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Defining Variables
+target_website="http://$(hostname -i)"
+target_webpage="content/about-us.php"
+target_content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
+
+#Fetching website output to a file
+curl "$target_website/$target_webpage" -o outfile
+
+#test case if pattern is matching
+if [[ "$(grep $target_content outfile)" ]];then
+  echo "success"
+  exit 0
+else
+  echo "failure"
+  exit 127
+fi
+
